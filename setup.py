c5e65cf4f5a733f38efe93bfc892a7a89b0b42c2
Felix B. Müller <57685553+felixbmuller@users.noreply.github.com>
2023-08-29 13:55:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='multipool',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description='MultiPool',
     author='Felix Benjamin Mueller',
     license='MIT',
